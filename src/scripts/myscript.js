9633707e01fa08dc8e69116c90d1917d22c1ef96
anjoalo1@gmail.com <anjojalo1@gmail.com>
2025-04-25 22:12:24 -0500

    changing code show

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -1,26 +1,21 @@
-import Prism from 'prismjs';
-import 'prism-themes/themes/prism-dracula.css';
-/* import 'prismjs/themes/prism-okaidia.css' */
-import 'prismjs/components/prism-css';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/plugins/custom-class/prism-custom-class';
+import Prism from "prismjs";
 
-import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
-import 'prismjs/plugins/toolbar/prism-toolbar.css';
-import 'prismjs/plugins/toolbar/prism-toolbar.js';
-import 'prismjs/plugins/download-button/prism-download-button.js';
-
-import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js';
+import "prism-themes/themes/prism-one-dark.css";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-kotlin";
+import "prismjs/plugins/custom-class/prism-custom-class";
+import "prismjs/plugins/line-numbers/prism-line-numbers";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/plugins/toolbar/prism-toolbar";
+import "prismjs/plugins/toolbar/prism-toolbar.css";
+import "prismjs/plugins/show-language/prism-show-language";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
+import "prismjs/plugins/inline-color/prism-inline-color";
+import "prismjs/plugins/inline-color/prism-inline-color.css";
+//match braces resaltar parentesis llaves
+import "prismjs/plugins/match-braces/prism-match-braces";
+import "prismjs/plugins/match-braces/prism-match-braces.css";
+/* import 'prismjs/plugins/previewers/prism-previewers'
+import 'prismjs/plugins/previewers/prism-previewers.css' */
+import "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js";
 Prism.highlightAll();
-Prism.plugins.NormalizeWhitespace.setDefaults({
-    'remove-trailing': true,
-    'remove-indent': true,
-    'left-trim': true,
-    'right-trim': true
-    /*'break-lines': 80,
-      'indent': 2,
-      'remove-initial-line-feed': false,
-      'tabs-to-spaces': 4,
-      'spaces-to-tabs': 4*/
-  });
